42dc9ce25a3b194eac5efb26b88b9dc8b60f6c52
Akshat Talwar <uniquedimension1@gmail.com>
2026-05-03 21:33:01 +0530

    updated config of env

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 
+HINDSIGHT_API_URL = os.getenv("HINDSIGHT_API_URL", "http://localhost:8888")
+HINDSIGHT_API_KEY = os.getenv("HINDSIGHT_API_KEY", None)
+
 if not all([GITHUB_TOKEN, GROQ_API_KEY, WEBHOOK_SECRET]):
     raise ValueError("Missing env vars. Check your .env file.")
